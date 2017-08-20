75d2b39aa5d067e321e4f13003d4fee01d32cccc
JAYASANKARAM\vbhar <v.bharathkumar@gmail.com>
2017-08-20 09:45:15 +0530

    Demo Data

--- src/providers/quote-data.ts
+++ src/providers/quote-data.ts
@@ -1,73 +1,11 @@
 import { Injectable } from '@angular/core';
-//import {AngularFireDatabaseModule} from 'angularfire2/database';
-
-import { Http } from '@angular/http';
-
-import { UserData } from './user-data';
-
-//import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
-
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class QuoteData {
-  data: any;
+   constructor(private _af: AngularFireDatabase) {}
 
-  constructor(/*private afd:AngularFireDatabaseModule,*/
-    public http: Http, public user: UserData) { }
-
-    getQuote(){
-        //return this.afd.list('/motivation-pyramid/quote/');
-    }
-
-  /*load(): any {
-    if (this.data) {
-      return Observable.of(this.data);
-    } else {
-      return this.http.get('assets/data/data.json')
-        .map(this.processData, this);
-    }
-  }
-
-  processData(data: any) {
-    // just some good 'ol JS fun with objects and arrays
-    // build up the data by linking speakers to sessions
-    this.data = data.json();
-
-    this.data.tracks = [];
-
-    // loop through each day in the schedule
-    this.data.schedule.forEach((day: any) => {
-      // loop through each timeline group in the day
-      day.groups.forEach((group: any) => {
-        // loop through each session in the timeline group
-        group.sessions.forEach((session: any) => {
-          session.speakers = [];
-          if (session.speakerNames) {
-            session.speakerNames.forEach((speakerName: any) => {
-              let speaker = this.data.speakers.find((s: any) => s.name === speakerName);
-              if (speaker) {
-                session.speakers.push(speaker);
-                speaker.sessions = speaker.sessions || [];
-                speaker.sessions.push(session);
-              }
-            });
-          }
-
-          if (session.tracks) {
-            session.tracks.forEach((track: any) => {
-              if (this.data.tracks.indexOf(track) < 0) {
-                this.data.tracks.push(track);
-              }
-            });
-          }
-        });
-      });
-    });
-
-    return this.data;
-  }*/
-
-  
+   public getQuotes(): FirebaseListObservable<any[]>{
+      return this._af.list('/quote');
+   }
 }
