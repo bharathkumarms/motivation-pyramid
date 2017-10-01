92c829553a84e69a9c51517ff29c2a23d313b650
JAYASANKARAM\vbhar <v.bharathkumar@gmail.com>
2017-10-01 10:40:55 +0530

    Added styles to speaker card

--- src/pages/motivation-speaker-list/motivation-speaker-list.ts
+++ src/pages/motivation-speaker-list/motivation-speaker-list.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import {
-  //ActionSheet,
+  ActionSheet,
   ActionSheetController,
-  //ActionSheetOptions,
+  ActionSheetOptions,
   Config,
   NavController
 } from 'ionic-angular';
@@ -10,12 +10,21 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 import { MotivationSpeakerData } from '../../providers/motivation-speaker-data';
 
+export interface ActionSheetButton {
+  text?: string;
+  role?: string;
+  icon?: string;
+  cssClass?: string;
+  handler?: () => boolean|void;
+};
+
 @Component({
   selector: 'page-motivation-speaker-list',
   templateUrl: 'motivation-speaker-list.html',
 })
 export class MotivationSpeakerListPage {
   speakers:any[]=[];
+  actionSheet: ActionSheet;
   constructor(
     public actionSheetCtrl: ActionSheetController,
     public navCtrl: NavController,
@@ -28,6 +37,67 @@ export class MotivationSpeakerListPage {
     this.motivationSpeakerData.getSpeakers().subscribe((speakers: any[]) => {
       this.speakers = speakers;
     });
+  }
+
+  goToSpeakerTwitter(speaker: any) {
+    this.inAppBrowser.create(
+      `https://twitter.com/${speaker.twitter}`,
+      '_blank'
+    );
+  }
+
+  openSpeakerShare(speaker: any) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Share ' + speaker.name,
+      buttons: [
+        {
+          text: 'Copy Link',
+          handler: () => {
+            console.log('Copy link clicked on https://twitter.com/' + speaker.twitter);
+            if ( (window as any)['cordova'] && (window as any)['cordova'].plugins.clipboard) {
+              (window as any)['cordova'].plugins.clipboard.copy(
+                'https://twitter.com/' + speaker.twitter
+              );
+            }
+          }
+        } as ActionSheetButton,
+        {
+          text: 'Share via ...'
+        } as ActionSheetButton,
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        } as ActionSheetButton
+      ]
+    } as ActionSheetOptions);
+
+    actionSheet.present();
+  }
+
+  openContact(speaker: any) {
+    let mode = this.config.get('mode');
+
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Contact ' + speaker.name,
+      buttons: [
+        {
+          text: `Email ( ${speaker.email} )`,
+          icon: mode !== 'ios' ? 'mail' : null,
+          handler: () => {
+            window.open('mailto:' + speaker.email);
+          }
+        } as ActionSheetButton,
+        {
+          text: `Call ( ${speaker.phone} )`,
+          icon: mode !== 'ios' ? 'call' : null,
+          handler: () => {
+            window.open('tel:' + speaker.phone);
+          }
+        } as ActionSheetButton
+      ]
+    } as ActionSheetOptions);
+
+    actionSheet.present();
   }
 
 }
